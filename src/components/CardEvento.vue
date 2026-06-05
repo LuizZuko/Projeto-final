656f004e78db1486e0bed23967810c0fbc10f500
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-06-05 20:18:47 -0300

    completa estrutura do card de evento com informações e ação. exibe nome, data e preço do evento formata preço com duas casas decimais adiciona botão de compra com emissão de evento finaliza estrutura do template do card

--- src/components/CardEvento.vue
+++ src/components/CardEvento.vue
@@ -3,3 +3,13 @@
     <img :src="evento.imagem" :alt="evento.nome" class="card-img" />
     <div class="card-content">
       <div>
+        <h3>{{ evento.nome }}</h3>
+        <p class="data">Data: {{ evento.data }}</p>
+        <p class="preco">R$ {{ evento.preco.toFixed(2) }}</p>
+      </div>
+      <button @click="$emit('abrir-modal', evento)" class="btn-comprar">
+        Comprar Ingresso
+      </button>
+    </div>
+  </div>
+</template>
