b67d5d427150720961b20584c03401ad38e2bf94
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-06-05 15:48:02 -0300

    adiciona listagem de eventos

--- src/components/PaginaFestas.vue
+++ src/components/PaginaFestas.vue
@@ -13,3 +13,19 @@
       <h3>Outras Festas & Baladas</h3>
       <input type="text" v-model="termoBusca" placeholder="Filtrar eventos..." class="input-busca-festas" />
     </section>
+
+    <section class="lista-horizontal-festas">
+      <p v-if="eventosFiltrados.length === 0" class="sem-resultado-festas">Nenhuma festa localizada.</p>
+      
+      <div v-for="evento in eventosFiltrados" :key="evento.id" class="festa-row-item">
+        <img :src="evento.imagem" :alt="evento.nome" class="row-img" />
+        <div class="row-info">
+          <h4>{{ evento.nome }}</h4>
+          <p class="row-data">📅 Data: {{ evento.data }}</p>
+          <p class="row-preco">R$ {{ evento.preco.toFixed(2) }}</p>
+        </div>
+        <button @click="$emit('abrir-modal', evento)" class="btn-comprar-row">Comprar</button>
+      </div>
+    </section>
+  </div>
+</template>
