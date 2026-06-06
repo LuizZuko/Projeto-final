2a9f87bd1bec7a0c829c50920d3b6f849ed6a4a1
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-06-06 10:39:53 -0300

    implementa form com prevenção de submit padrão cria campo para nome completo com v-model adiciona validação obrigatória e placeholder organiza campo dentro de form-group

--- src/components/ModalCompra.vue
+++ src/components/ModalCompra.vue
@@ -4,3 +4,9 @@
       <h2>Comprar Ingresso</h2>
       <p style="margin-top: 12px;"><strong>Evento:</strong> {{ eventoSelecionado.nome }}</p>
       <p><strong>Valor Unitário:</strong> R$ {{ eventoSelecionado.preco.toFixed(2) }}</p>
+
+      <form @submit.prevent="finalizarCompra">
+        <div class="form-group">
+          <label for="nome">Nome Completo:</label>
+          <input type="text" id="nome" v-model="nomeComprador" required placeholder="Digite seu nome" />
+        </div>
