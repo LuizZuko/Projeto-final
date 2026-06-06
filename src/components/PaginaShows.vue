d567b017a36881b97fbb5342526c57d3884f2c1d
LuizZuko <Luizhenriquezukovskimangini@gmail.com>
2026-06-06 12:36:44 -0300

    Script da Pagina de Shows do projeto

---
+++ src/components/PaginaShows.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, computed } from 'vue';
+import CardEvento from './CardEvento.vue';
+
+defineEmits(['abrir-modal']);
+const termoBusca = ref('');
+
+const shows = ref([
+  { 
+    id: 1, 
+    nome: 'Os Barões da Pisadinha', 
+    data: '15/10/2026', 
+    preco: 120.00, 
+    imagem:  
+},
+
+  { 
+    id: 2, 
+    nome: 'João Gomes', 
+    data: '22/10/2026', 
+    preco: 150.00, 
+    imagem:  
+},
+
+  { 
+    id: 3,
+    nome: 'Tarcísio do Acordeon', 
+    data: '05/11/2026', 
+    preco: 90.00, 
+    imagem:  
+},
+
+  { 
+    id: 4, 
+    nome: 'Nattan', 
+    data: '12/11/2026', 
+    preco: 180.00, 
+    imagem:  
+}
+
+]);
+
+const eventosFiltrados = computed(() => {
+  return shows.value.filter(evento => evento.nome.toLowerCase().includes(termoBusca.value.toLowerCase()));
+});
+</script>
